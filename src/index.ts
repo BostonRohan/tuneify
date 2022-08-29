b61abc7c957d655dbd00a654c27cd488f273128d
Boston Rohan <bostonrohan@gmail.com>
2022-08-29 18:11:07 -0400

    added token

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { Client } from "discord.js";
 
+require("dotenv").config();
+
 console.log("Bot is starting...");
 
 const client = new Client({
   intents: [],
 });
+
+client.login(process.env.token);
 
 console.log(client);
