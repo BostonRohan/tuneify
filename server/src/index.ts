f120a69ededbab568588e2e252eaf060404a0fde
Boston Rohan <bostonrohan@gmail.com>
2022-09-01 12:09:24 -0400

    added redis

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routes/router";
+import { createClient } from "redis";
 
 dotenv.config();
 
 const app = express();
+
+//redis
+export const client = createClient();
+
+(async () => {
+  client.on("error", (err) => console.log("Redis Client Error", err));
+
+  await client.connect();
+})();
 
 //routes
 app.use(router);
@@ -12,5 +22,3 @@ app.use(router);
 app.listen(8888, () => {
   console.log(`server started at http://localhost:${8888}`);
 });
-
-export default app;
