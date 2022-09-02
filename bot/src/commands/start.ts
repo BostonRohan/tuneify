aacca1798d53b0406194eb81ef630d950f77476e
Boston Rohan <bostonrohan@gmail.com>
2022-09-02 18:26:08 -0400

    created loggedin follow-up

--- bot/src/commands/start.ts
+++ bot/src/commands/start.ts
@@ -4,15 +4,16 @@ import {
   ButtonStyle,
   Client,
   CommandInteraction,
+  EmbedBuilder,
 } from "discord.js";
 import url from "../utils/authUrl";
 import { Command } from "../commands";
+import axios from "axios";
 
 export const Start: Command = {
   name: "start",
   description: "start by logging into your spotify account.",
   run: async (client: Client, interaction: CommandInteraction) => {
-    //type for row??
     const row: any = new ActionRowBuilder().addComponents(
       new ButtonBuilder()
         .setURL(url)
@@ -25,5 +26,31 @@ export const Start: Command = {
       content: "continue signing into your spotify account.",
       components: [row],
     });
+
+    const {
+      data: { display_name, images, external_urls, error },
+    } = await axios.get("http://localhost:8888/loggedin");
+
+    if (error) {
+      await interaction.followUp({
+        ephemeral: true,
+        content: "there was an error logging you in, please try again.",
+      });
+    } else {
+      const accountEmbed = new EmbedBuilder()
+        .setColor(0xdb954)
+        .setAuthor({
+          name: display_name,
+          iconURL: images[0].url,
+          url: external_urls.spotify,
+        })
+        .setTimestamp();
+
+      await interaction.followUp({
+        ephemeral: true,
+        content: "you are now logged in!",
+        embeds: [accountEmbed],
+      });
+    }
   },
 };
