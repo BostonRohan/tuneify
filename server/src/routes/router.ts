680cfdc3d79ac4b3ef704914f2f3c502a0486212
Boston Rohan <bostonrohan@gmail.com>
2022-09-07 16:57:17 -0400

    created spotify auth endpoint

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import discordAuth from "../controllers/auth/discord";
+import spotifyAuth from "../controllers/auth/spotify";
 
 const router = express.Router();
 
 //endpoints
 router.get("/discord/auth", discordAuth);
+router.get("/spotify/auth", spotifyAuth);
 
 export default router;
