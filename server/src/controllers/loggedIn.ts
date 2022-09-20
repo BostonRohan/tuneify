22ccdfec8193de35b5941f121833af87de4e49d7
Boston Rohan <bostonrohan@gmail.com>
2022-09-20 13:15:36 -0400

    sytactic sugar

--- server/src/controllers/loggedIn.ts
+++ server/src/controllers/loggedIn.ts
@@ -5,8 +5,8 @@ const loggedIn = async (_req: Request, res: Response) => {
   try {
     const { data } = await axios.get("https://api.spotify.com/v1/me");
     res.send(data);
-  } catch (err) {
-    res.send({ error: err });
+  } catch (error) {
+    res.send({ error });
   }
 };
 
