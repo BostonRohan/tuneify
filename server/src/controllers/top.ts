22b58215ad22e4656a19ef79815e59eeff1cd334
Boston Rohan <bostonrohan@gmail.com>
2022-09-21 17:51:10 -0400

    changed limit to 3

--- server/src/controllers/top.ts
+++ server/src/controllers/top.ts
@@ -5,7 +5,7 @@ const top = async (req: Request, res: Response) => {
   const { type, time_range } = req.body;
   try {
     const { data } = await axios.get(
-      `https://api.spotify.com/v1/me/top/${type}?time_range=${time_range}&limit=10`
+      `https://api.spotify.com/v1/me/top/${type}?time_range=${time_range}&limit=3`
     );
     res.send(data);
   } catch (error) {
