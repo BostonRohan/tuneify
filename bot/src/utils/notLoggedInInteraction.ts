fca37a71b708b3a65e35fa6e5d0e6cf588ab4e67
Boston Rohan <bostonrohan@gmail.com>
2022-10-10 20:09:51 -0400

    quality of life fixes

--- bot/src/utils/notLoggedInInteraction.ts
+++ bot/src/utils/notLoggedInInteraction.ts
@@ -2,7 +2,7 @@ import { ChatInputCommandInteraction } from "discord.js";
 
 export default async (interaction: ChatInputCommandInteraction) => {
   return await interaction.followUp({
+    content: "you are not logged in please retry the `/start` command.",
     ephemeral: true,
-    content: "there was an error running this command, please try again.",
   });
 };
