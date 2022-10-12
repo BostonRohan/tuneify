2ecbfe7f0d78ba12005c076b2468ab1be3663291
Boston Rohan <bostonrohan@gmail.com>
2022-10-12 12:32:14 -0400

    added limit

--- server/src/controllers/recentlyPlayed.ts
+++ server/src/controllers/recentlyPlayed.ts
@@ -4,7 +4,7 @@ import { Request, Response } from "express";
 const recentlyPlayed = async (_req: Request, res: Response) => {
   try {
     const { data } = await axios.get(
-      `https://api.spotify.com/v1/me/player/recently-played`
+      `https://api.spotify.com/v1/me/player/recently-played?limit=10`
     );
     res.send(data);
   } catch (error) {
