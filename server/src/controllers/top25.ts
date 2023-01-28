61e9ee1c0301c6f60fb46fdb1ff44d7f8c337e03
bostonrohan <bostonrohan@gmail.com>
2023-01-28 10:59:56 -0500

    return boolean from top25

--- server/src/controllers/top25.ts
+++ server/src/controllers/top25.ts
@@ -2,10 +2,17 @@
 import { Request, Response } from "express";
 import { prisma } from "../index";
 
-const top25 = async (_req: Request, res: Response) => {
+const top25 = async (req: Request, res: Response) => {
+  const {
+    body: { discord_id },
+  } = req;
   try {
     const users = await prisma.user.findMany({select: {discord_id: true}, orderBy: {requests: 'desc'}});
-    res.send(users.slice(0, 24));
+
+    const top25 = users.find((user) => user.discord_id === discord_id);
+
+    res.send({data: top25 ? true : false})
+    
   } catch (error) {
     res.send({ error });
   }
