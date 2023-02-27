28e2cf921a907c7185f4ba1cba63607b747b8589
bostonrohan <bostonrohan@gmail.com>
2023-02-26 20:06:08 -0500

    remove unneeded files

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -1,13 +1,8 @@
 import { Client } from "discord.js";
 import interactionCreate from "./listeners/interactionCreate";
 import ready from "./listeners/ready";
-import server from "./server";
 
 require("dotenv").config();
-
-console.log("Bot is starting...");
-
-server();
 
 const client = new Client({
   intents: [],
